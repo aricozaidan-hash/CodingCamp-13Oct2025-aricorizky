8a01ec526fd6f4c695753be85b49469a83a7767c
aricozaidan-hash <aricozaidan@gmail.com>
2025-10-18 09:05:06 +0700

    Final Web

--- js/script.js
+++ js/script.js
@@ -1,53 +1,49 @@
-/// Database Simulation
+
 let tasksDb = [];
 
-/// Add Functionality
 function addTask() {
-    /// Get Input Values
     const taskInput = document.getElementById('todo-input');
     const taskDate = document.getElementById('todo-date');
 
-    /// Validate Input
-    if (validateInput(taskInput.value, taskDate.value)) {
-        /// Create Task Object
+    if (validateInput(taskInput.value, taskDate.value)) {        /// Create Task Object
         const newTask = {
             task: taskInput.value,
             date: taskDate.value,
         }
-
-        /// Add to database
         tasksDb.push(newTask);
 
-        /// Render
         renderTasks();
     }
 }
 
-/// Render Functionality
 function renderTasks() {
-    /// Clear Existing List
+    
     const taskList = document.getElementById('task-list');
     taskList.innerHTML = '';
 
-    /// Render Each Task
-    tasksDb.forEach((taskObj, index) => {
-        taskList.innerHTML += `<li>${taskObj.task} - ${taskObj.date}</li>`;
-    });
+    if (tasksDb.length === 0) {
+        taskList.innerHTML = 'No tasks added';
+    } else {
+        tasksDb.forEach((taskObj, index) => {
+            taskList.innerHTML += `<li>${taskObj.task} - ${taskObj.date}</li>`;
+        });
+    }
 }
 
-/// Delete All Functionality
 function deleteAllTasks() {
     /// Clear Database
     tasksDb = [];
 
-    /// Render
+    
     renderTasks();
 }
 
-/// Filter Functionality (Placeholder)
-function filterTasks() { }
 
-/// Input Validation
+function filterTasks() {
+    
+}
+
+
 function validateInput(task, date) {
     /// Simple Validation
     if (task.trim() === '' || date.trim() === '') {
